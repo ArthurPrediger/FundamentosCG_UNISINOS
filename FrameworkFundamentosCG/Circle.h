f00ef69f66f75fc723432b00ca4a83b715563a7a
Arthur_Prediger <arthurpredigerrambo@gmail.com>
2022-03-26 00:53:43 -0300

    Half screen width and height for the circle class are now precalculated for the calculations of vertices coordinates

--- FrameworkFundamentosCG/Circle.h
+++ FrameworkFundamentosCG/Circle.h
@@ -8,18 +8,21 @@ class Circle
 public:
 	static std::vector<float> Make(float radius, std::pair<float, float> center, int width, int height, int nVertices = 20)
 	{
+		const float halfWidth = width / 2.0f;
+		const float halfHeight = height / 2.0f;
+
 		std::vector<float> vertices;
 		vertices.reserve(float(nVertices + 2) * 3);
 
-		vertices.push_back(center.first / (width / 2.0f));
-		vertices.push_back(center.second / (height / 2.0f));
+		vertices.push_back(center.first / halfWidth);
+		vertices.push_back(center.second / halfHeight);
 		vertices.push_back(0.0f);
 
 		const float angle = 2 * PI / (float)nVertices;
 		for (float i = 0; i < nVertices; i++)
 		{
-			float x = (cos(i * angle) * radius + center.first) / (width / 2.0f);
-			float y = (sin(i * angle) * radius + center.second) / (height / 2.0f);
+			float x = (cos(i * angle) * radius + center.first) / halfWidth;
+			float y = (sin(i * angle) * radius + center.second) / halfHeight;
 			vertices.push_back(x);
 			vertices.push_back(y);
 			vertices.push_back(0.0f);
