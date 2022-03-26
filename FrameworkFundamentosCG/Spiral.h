a988fd64cf2f790896037c2491a07ae745ec6b0c
Arthur_Prediger <arthurpredigerrambo@gmail.com>
2022-03-26 00:45:50 -0300

    Fixed scale for the spiral class (divided screen width and height by 2).

--- FrameworkFundamentosCG/Spiral.h
+++ FrameworkFundamentosCG/Spiral.h
@@ -8,14 +8,16 @@ static constexpr float PI = 3.14159265359;
 class Spiral
 {
 public:
-	static std::vector<float> Make(int width, int height, float maxRadius = 300.0f, int nLaps = 3, 
+	static std::vector<float> Make(int width, int height, float maxRadius = 150.0f, int nLaps = 3, 
 		std::pair<float, float> center = { 0.0f, 0.0f }, int nVertices = 500)
 	{
+		const float halfWidth = width / 2.0f;
+		const float halfHeight = height / 2.0f;
 		std::vector<float> vertices;
 		vertices.reserve(float(nVertices + 1) * 3);
 
-		vertices.push_back(center.first / width);
-		vertices.push_back(center.second / height);
+		vertices.push_back(center.first / halfWidth);
+		vertices.push_back(center.second / halfHeight);
 		vertices.push_back(0.0f);
  
 		const float dTheta = 2 * PI * nLaps / (float)nVertices;
@@ -24,8 +26,8 @@ public:
 		for (int i = 0; i < nVertices; i++, radius += dRadius)
 		{
 			const float angle = i * dTheta;
-			float x = (cos(angle) * radius + center.first) / width;
-			float y = (sin(angle) * radius + center.second) / height;
+			float x = (cos(angle) * radius + center.first) / halfWidth;
+			float y = (sin(angle) * radius + center.second) / halfHeight;
 			vertices.push_back(x);
 			vertices.push_back(y);
 			vertices.push_back(0.0f);
