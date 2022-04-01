0b0ea8ec3055f4d83bda3358a958d25bf057f9be
Arthur_Prediger <arthurpredigerrambo@gmail.com>
2022-04-01 12:01:30 -0300

    Inclusão da biblioteca glm

--- FrameworkFundamentosCG/Source.cpp
+++ FrameworkFundamentosCG/Source.cpp
@@ -4,6 +4,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"
+#include <glm/glm.hpp> 
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Protótipo da função de callback de teclado
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
